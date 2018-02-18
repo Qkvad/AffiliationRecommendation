015b398a67e837748d2198c4c176742dad9f28cb
ivceh <ivan.ceh1234@gmail.com>
2018-02-18 16:26:03 +0100

    Adaptation of output in LatentFactors function

--- octave/LatentFactors.m
+++ octave/LatentFactors.m
@@ -1,9 +1,16 @@
-function [final] = LatentFactors (A,A1,S,lambda,d)
+function [final] = LatentFactors (A,A1,S,lambda,d,c)
   [nu,ng] = size(A);
   C1 = zeros(nu+ng,nu+ng);
   C1(1:nu, 1:nu) = lambda * S;
   C1(1:nu, nu+1:nu+ng) = A;
   C1(nu+1:nu+ng, 1:nu) = A1;
   [U,sigma,V] = svd(C1);
-  final = U(1:nu,1:d)*sigma(1:d,1:d)*V(nu+1:nu+ng,1:d).';
+  Capprox = U(1:nu,1:d)*sigma(1:d,1:d)*V(nu+1:nu+ng,1:d).';
+  [rowsum,colsum]=size(Capprox);
+  for i=1:rowsum
+    [sorted,index]=sort(Capprox(i,:),'descend');
+    for j=1:c
+      final(i,j)=index(j);
+    end
+  end
 endfunction
